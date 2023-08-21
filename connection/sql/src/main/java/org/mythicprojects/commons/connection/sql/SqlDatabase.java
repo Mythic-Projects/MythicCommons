775ecb9e45f6c54f299ce3073f707c5300b09c75
Peridot <peridot491@protonmail.com>
2023-08-21 14:03:05 +0200

    Add methods to connect/execute statements sql async

--- connection/sql/src/main/java/org/mythicprojects/commons/connection/sql/SqlDatabase.java
+++ connection/sql/src/main/java/org/mythicprojects/commons/connection/sql/SqlDatabase.java
@@ -12,18 +12,22 @@ import org.jetbrains.annotations.NotNull;
 import org.mythicprojects.commons.connection.Connection;
 import org.mythicprojects.commons.connection.sql.util.DatabaseHelper;
 import org.mythicprojects.commons.connection.sql.util.SqlConsumer;
+import org.mythicprojects.commons.util.Validate;
 
 public class SqlDatabase implements Connection {
 
     private final SqlConfiguration configuration;
     private final Consumer<Throwable> exceptionHandler;
+    private final Consumer<Runnable> runAsync;
+
     private HikariDataSource dataSource;
 
     private final Map<String, SqlTable> tables = new ConcurrentHashMap<>();
 
-    public SqlDatabase(@NotNull SqlConfiguration configuration, @NotNull Consumer<Throwable> exceptionHandler) {
-        this.configuration = configuration;
-        this.exceptionHandler = exceptionHandler;
+    public SqlDatabase(@NotNull SqlConfiguration configuration, @NotNull Consumer<Throwable> exceptionHandler, @NotNull Consumer<Runnable> runAsync) {
+        this.configuration = Validate.notNull(configuration, "configuration cannot be null");
+        this.exceptionHandler = Validate.notNull(exceptionHandler, "exceptionHandler cannot be null");
+        this.runAsync = Validate.notNull(runAsync, "runAsync cannot be null");
     }
 
     public @NotNull String prepareTableName(@NotNull String baseName) {
@@ -64,6 +68,10 @@ public class SqlDatabase implements Connection {
         }
     }
 
+    public void connectAsync(@NotNull SqlConsumer<java.sql.Connection> connectionConsumer) {
+        this.runAsync(() -> this.connect(connectionConsumer));
+    }
+
     public void executeStatement(@NotNull SqlConsumer<PreparedStatement> statementConsumer, @NotNull String query) {
         this.connect(connection -> {
             try (PreparedStatement statement = connection.prepareStatement(query)) {
@@ -74,12 +82,24 @@ public class SqlDatabase implements Connection {
         });
     }
 
+    public void executeStatementAsync(@NotNull SqlConsumer<PreparedStatement> statementConsumer, @NotNull String query) {
+        this.runAsync(() -> this.executeStatement(statementConsumer, query));
+    }
+
     public void executeStatement(@NotNull SqlConsumer<PreparedStatement> statementConsumer, @NotNull String query, @NotNull String tableName) {
         this.executeStatement(statementConsumer, DatabaseHelper.replaceTable(query, tableName));
     }
 
+    public void executeStatementAsync(@NotNull SqlConsumer<PreparedStatement> statementConsumer, @NotNull String query, @NotNull String tableName) {
+        this.runAsync(() -> this.executeStatement(statementConsumer, query, tableName));
+    }
+
     public void executeStatementWithPreparedTable(@NotNull SqlConsumer<PreparedStatement> statementConsumer, @NotNull String query, @NotNull String baseTableName) {
         this.executeStatement(statementConsumer, query, this.prepareTableName(baseTableName));
+    }
+
+    public void executeStatementWithPreparedTableAsync(@NotNull SqlConsumer<PreparedStatement> statementConsumer, @NotNull String query, @NotNull String baseTableName) {
+        this.runAsync(() -> this.executeStatementWithPreparedTable(statementConsumer, query, baseTableName));
     }
 
     public Optional<SqlTable> findTable(@NotNull String baseName) {
@@ -102,6 +122,10 @@ public class SqlDatabase implements Connection {
     @Blocking
     public SqlTable findOrCreateTable(@NotNull String baseName, @NotNull String createQuery) {
         return this.findTable(baseName).orElseGet(() -> this.createTable(baseName, createQuery));
+    }
+
+    public void runAsync(@NotNull Runnable runnable) {
+        this.runAsync.accept(runnable);
     }
 
 }

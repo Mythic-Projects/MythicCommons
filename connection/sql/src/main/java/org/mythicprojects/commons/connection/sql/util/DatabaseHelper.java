baaf80726c05c2e77b8a27fd2df71bf04581f7e5
Peridot <peridot491@protonmail.com>
2024-03-26 15:02:21 +0100

    Add methods to DatabaseHelper to simplify process of processing query with multiple results

--- connection/sql/src/main/java/org/mythicprojects/commons/connection/sql/util/DatabaseHelper.java
+++ connection/sql/src/main/java/org/mythicprojects/commons/connection/sql/util/DatabaseHelper.java
@@ -5,7 +5,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -70,6 +75,30 @@ public final class DatabaseHelper {
     public static @Nullable Instant getInstant(@NotNull ResultSet resultSet, @NotNull String columnName) throws SQLException {
         Timestamp timestamp = resultSet.getTimestamp(columnName);
         return timestamp == null ? null : timestamp.toInstant();
+    }
+
+    public static <V> @NotNull List<V> processResultSetToList(@NotNull ResultSet resultSet, @NotNull SqlFunction<ResultSet, V> resultProcessor) throws SQLException {
+        List<V> results = new ArrayList<>();
+        while (resultSet.next()) {
+            results.add(resultProcessor.apply(resultSet));
+        }
+        return results;
+    }
+
+    public static <V> @NotNull Set<V> processResultSetToSet(@NotNull ResultSet resultSet, @NotNull SqlFunction<ResultSet, V> resultProcessor) throws SQLException {
+        Set<V> results = new LinkedHashSet<>();
+        while (resultSet.next()) {
+            results.add(resultProcessor.apply(resultSet));
+        }
+        return results;
+    }
+
+    public static <K, V> @NotNull Map<K, V> processResultSetToMap(@NotNull ResultSet resultSet, @NotNull SqlFunction<ResultSet, K> keyProcessor, @NotNull SqlFunction<ResultSet, V> valueProcessor) throws SQLException {
+        Map<K, V> results = new LinkedHashMap<>();
+        while (resultSet.next()) {
+            results.put(keyProcessor.apply(resultSet), valueProcessor.apply(resultSet));
+        }
+        return results;
     }
 
 }
